5fc8f5db7dfde88419ceeb3f99786866fe47e02c
Robin Kim <rkim1214@gmail.com>
2018-09-20 14:22:37 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+ var arrayWithNewBeginning = [element, ...array];
+ return arrayWithNewBeginning;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  var arrayWithNewEnd = [...array, element];
+  return arrayWithNewEnd;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
